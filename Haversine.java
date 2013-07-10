f6b21167c70d620c034c59d8198434db3b4c8d68
Jason Winn <jasonmwinn@gmail.com>
2013-07-10 16:10:07 -0700

    Create Haversine.java

---
+++ Haversine.java
@@ -0,0 +1,38 @@
+/**
+ * Jason Winn
+ * http://jasonwinn.org
+ * Created July 10, 2013
+ *
+ * Description: Small class that provides approximate distance between
+ * two points using the Haversine function.
+ *
+ * Call in a static context:
+ * Haversine.distance(47.6788206, -122.3271205,
+ *                    47.6788206, -122.5271205)
+ * --> 14.973190481586224 [km]
+ *
+ */
+
+public class Haversine {
+    private static final int EARTH_RADIUS = 6371; // Approx Earth radius in KM
+
+    public static double distance(double startLat, double startLong,
+                                  double endLat, double endLong) {
+
+        startLat = Math.toRadians(startLat);
+        endLat   = Math.toRadians(endLat);
+        double dLat  = Math.toRadians(endLat - startLat);
+        double dLong = Math.toRadians(endLong - startLong);
+
+        double a = haversin(dLat) + haversin(dLong) *
+                   Math.cos(startLat) * Math.cos(endLat);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return EARTH_RADIUS * c; // <-- d
+    }
+
+    public static double haversin(double val) {
+        return Math.pow(Math.sin(val / 2), 2);
+    }
+}
