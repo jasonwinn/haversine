76315fe5caedc8005b9366cbd5b619e33cf77331
Jason Winn <jasonmwinn@gmail.com>
2013-07-11 15:55:38 -0700

    Calculate dLat/dLong before converting startLat/endLat into radians.

--- Haversine.java
+++ Haversine.java
@@ -19,15 +19,14 @@ public class Haversine {
     public static double distance(double startLat, double startLong,
                                   double endLat, double endLong) {
 
+        double dLat  = Math.toRadians((endLat - startLat));
+        double dLong = Math.toRadians((endLong - startLong));
+
         startLat = Math.toRadians(startLat);
         endLat   = Math.toRadians(endLat);
-        double dLat  = Math.toRadians(endLat - startLat);
-        double dLong = Math.toRadians(endLong - startLong);
 
-        double a = haversin(dLat) + haversin(dLong) *
-                   Math.cos(startLat) * Math.cos(endLat);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double a = haversin(dLat) + Math.cos(startLat) * Math.cos(endLat) * haversin(dLong);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
         return EARTH_RADIUS * c; // <-- d
     }
